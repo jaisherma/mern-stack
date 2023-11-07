561cebd9de710d9930cba6ea8fcfb632de1dd091
jaisherma <79637980+jaisherma@users.noreply.github.com>
2023-11-07 16:41:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://db/amazona');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('DB is connected'))
+.catch(err => console.error(err));
 app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
