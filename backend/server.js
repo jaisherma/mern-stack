5559a45d268369f8b119f58ec9988288c286b3ff
jaisherma <79637980+jaisherma@users.noreply.github.com>
2023-11-07 16:56:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 
 const mongoose = require('mongoose');
 
-const mongoose = require('mongoose');
-
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
